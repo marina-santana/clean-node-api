75179e30443c420347b677397e40e63a99939401
Marina Santana <marinasantana7894@gmail.com>
2020-12-23 10:41:56 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,9 @@
+import { EmailValidatorApapter } from './email-validator'
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorApapter()
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
